f3f6dcaf9492397787847355a9ade752bf4c77ec
michaldrozdz <mich0232@gmail.com>
2022-09-17 19:47:39 +0200

    Create base manager

---
+++ src/commands.py
@@ -0,0 +1,70 @@
+import hashlib
+
+import click
+
+from helpers import init, read_store, write_store, store_credentials_file, use_credentials_file
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command('init')
+def initialize():
+    init()
+
+@click.command("add")
+@click.argument('path', type=click.Path())
+@click.argument('alias', type=str)
+def add_file(path, alias: str):
+    with open(path, 'rb') as file:
+        content = file.read()
+        hex_digest = str(hashlib.md5(content).hexdigest())
+
+        store_credentials_file(file_hash=hex_digest, content=content)
+        write_store(**{f"alias-{alias}":hex_digest})
+
+    click.echo(f"File stored successfully")
+
+
+@click.command('list')
+def list_elements():
+    elements = read_store()
+    for k, v in elements.items():
+        if k.startswith('alias-'):
+            click.echo(k.replace('alias-', ''))
+
+
+@click.command()
+@click.argument("alias", type=str)
+def remove_file(alias):
+    elements = read_store()
+    for k, v in elements.items():
+        if k == f"alias-{alias}":
+            # TODO: Ability to remove from store
+            pass
+
+
+@click.command("use")
+@click.argument("alias", type=str)
+def use_alias(alias):
+    elements = read_store()
+    hash = None
+    for k, v in elements.items():
+        if k == f"alias-{alias}":
+            hash = v
+            break
+
+    use_credentials_file(file_hash=hash)
+    click.echo(f"Now using: {alias}")
+
+
+cli.add_command(initialize)
+cli.add_command(add_file)
+cli.add_command(list_elements)
+cli.add_command(use_alias)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ src/exceptions.py
@@ -0,0 +1,3 @@
+
+class AWSProfileManagerException(Exception):
+    pass
