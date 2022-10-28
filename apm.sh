a4d7558d66488254f536deb90eaccf63eacef516
michaldrozdz <mich0232@gmail.com>
2022-10-28 11:18:00 +0200

    Rename Clouder

--- apm.sh
+++ apm.sh
@@ -1,3 +1,3 @@
-CLOUDER_DIR=$(dirname -- ${BASH_SOURCE[0]})
-source $CLOUDER_DIR/venv/bin/activate
-python $CLOUDER_DIR/src/commands.py $@
+APM_DIR=$(dirname -- ${BASH_SOURCE[0]})
+source $APM_DIR/venv/bin/activate
+python $APM_DIR/src/commands.py $@
